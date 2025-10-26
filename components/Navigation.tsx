d8578577547934c17d01a414aa24dbf74c99c2e2
FeltSense <feltsense62@gmail.com>
2025-10-26 20:58:55 -0300

    Auto-fix build errors

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import React from 'react';
+
 export default function Navigation() {
   const [activeMenu, setActiveMenu] = React.useState<string | null>(null);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
